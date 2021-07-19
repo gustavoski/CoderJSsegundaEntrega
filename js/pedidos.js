330776ae8196c094b3299520ccd3cd8f2d0d92a9
Gustavo <gustavo.skidelsky.37@gmail.com>
2021-07-19 19:13:41 -0300

    init

---
+++ js/pedidos.js
@@ -0,0 +1,24 @@
+
+
+//definición de funciones y selección de elementos del html
+const carro = new Carrito();
+const carrito = document.getElementById('carrito');
+const productos = document.getElementById('lista-productos');
+const listaProductos = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+const procesarPedidoBtn = document.getElementById('procesar-pedido');
+
+
+//carga los eventos del proyecto
+cargarEventos();
+
+function cargarEventos() {
+    productos.addEventListener('click', (e) => {carro.comprarProducto(e)});
+    carrito.addEventListener('click', (e) =>{carro.eliminarProducto(e)});
+    vaciarCarritoBtn.addEventListener('click', (e) =>{carro.vaciarCarrito(e)});
+    document.addEventListener('DOMContentLoaded', carro.leerLocalStorage());
+
+    procesarPedidoBtn.addEventListener('click', (e)=>{carro.procesarPedido(e)});
+
+
+}
